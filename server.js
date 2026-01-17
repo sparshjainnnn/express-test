ac5756d59df2eeeeb7e264f35a3e5bdac0743d4f
sparshjainnnn <sparshrajjain@gmail.com>
2026-01-17 11:37:03 +0530

    bye world

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World 👋");
+  res.send("Bye world");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
